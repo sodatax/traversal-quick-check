66f98ad6ce188b895faab06df7c03aa7035d3b5b
sodatax <h.nacanaynay@gmail.com>
2026-06-07 15:09:17 -0700

    completed the quck check

--- src/QuickCheck.java
+++ src/QuickCheck.java
@@ -15,6 +15,14 @@ public class QuickCheck {
    * @param node the root node of the binary tree
    */
   public static void printLongerThan7(TreeNode<String> node) {
+    if(node==null) return;
+
+    if(node.value.length() > 7){
+      System.out.println(node.value);
+    }
+    printLongerThan7(node.left);
+    printLongerThan7(node.right);
+
   }
 
   /**
@@ -28,6 +36,11 @@ public class QuickCheck {
    * @return the sum of all odd-valued nodes
    */
   public static int oddSum(TreeNode<Integer> node) {
-      return -1;
+    if(node==null) return 0;
+    int isOdd = 0;
+    if(node.value%2!=0){
+      isOdd=node.value;
+    }
+    return isOdd + oddSum(node.left) + oddSum(node.right);
   }
 }
